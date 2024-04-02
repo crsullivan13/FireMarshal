e1c5d7a40a6c44db2f84bbd71aa20fc0c011e6b9
Connor Sullivan <connor.r.sullivan9922@gmail.com>
2024-04-02 17:10:54 -0500

    make sure csv outputs are correct

--- boards/firechip/base-workloads/cache-test/overlay/root/Attacks/AggregateBw.sh
+++ boards/firechip/base-workloads/cache-test/overlay/root/Attacks/AggregateBw.sh
@@ -8,9 +8,9 @@ set -euo pipefail
 chmod +x BothBankBw.sh SingleBankBw.sh
 
 for i in 1 2 3 4; do ./SingleBankBw.sh 0 $i ; done
-cat outputs/bw/singlebank* | grep -i sum | awk 'NF{print $(NF)}' > outputs/bw/singlebankbw-c1234-bank0.sh
+cat outputs/bw/singlebank* | grep -i sum | awk 'NF{print $(NF)}' > outputs/bw/singlebankbw-c1234-bank0.csv
 
 sleep 2
 
 for i in 1 2 3 4; do ./BothBankBw.sh $i ; done
-cat outputs/bw/bothbank* | grep -i sum | awk 'NF{print $(NF)}' > outputs/bw/bothbankbw-c1234-bank0.sh
+cat outputs/bw/bothbank* | grep -i sum | awk 'NF{print $(NF)}' > outputs/bw/bothbankbw-c1234.csv
