12f214172e0b40b86e4262534a6df8e8d9a87e85
Connor Sullivan <connor.r.sullivan9922@gmail.com>
2024-08-14 12:18:01 -0500

    Clean out guest-init

--- boards/firechip/base-workloads/cache-test/guest-init.sh
+++ boards/firechip/base-workloads/cache-test/guest-init.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
 # set -x
 
-# echo "Installing killall"
-# sudo yum install -y psmisc
-
-# echo "Installing htop"
-# sudo dnf install -y htop
-
-# echo "Installing bc"
-# sudo dnf -y install bc
-
 poweroff
