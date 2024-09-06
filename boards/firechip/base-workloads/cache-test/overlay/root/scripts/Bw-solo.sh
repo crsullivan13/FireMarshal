471e826d95ec0bccd536c15f0136e8aad9f53205
Connor Sullivan <connor.r.sullivan9922@gmail.com>
2024-09-06 16:26:51 -0500

    Fix bw workload script

--- boards/firechip/base-workloads/cache-test/overlay/root/scripts/Bw-solo.sh
+++ boards/firechip/base-workloads/cache-test/overlay/root/scripts/Bw-solo.sh
@@ -5,4 +5,4 @@ set -euo pipefail
 OUT=$1
 
 sleep 0.5
-Bw -c 0 -m 128 -i 15000 -x | grep B/W | awk 'NF{print $(NF-7)}' > ../outputs/$1
+Bw -c 0 -m 128 -i 15000 | grep B/W | awk 'NF{print $(NF-7)}' > ../outputs/$1
